87e231e0b357a24ed895cf100b214ff3400c98e9
santiago <santiagoantoniomariscal@gmail.com>
2019-07-21 12:26:40 -0500

    samv

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams,ToastController,LoadingController } from 'ionic-angular';
+import { NavController, NavParams, ToastController, LoadingController } from 'ionic-angular';
 import { TabsPage } from '../tabs/tabs';
 import { UsuariosProvider } from '../../providers/usuarios/usuarios';
 import { SucursalesProvider } from '../../providers/sucursales/sucursales';
@@ -12,13 +12,13 @@ import { Storage } from '@ionic/storage';
   templateUrl: 'login.html',
 })
 export class LoginPage {
-  public invalido:boolean = false;
+  public invalido: boolean = false;
   public arreglo = [];
-  public sucursal:any = "";
+  public sucursal: any = "";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,private storage: Storage,
-  private toasCtrl:ToastController,private usuariosPdr:UsuariosProvider,private loadCtrl:LoadingController,
-  private sucursalesPrd:SucursalesProvider) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private storage: Storage,
+    private toasCtrl: ToastController, private usuariosPdr: UsuariosProvider, private loadCtrl: LoadingController,
+    private sucursalesPrd: SucursalesProvider) {
   }
 
   ionViewDidLoad() {
@@ -27,70 +27,69 @@ export class LoginPage {
 
   ionViewDidEnter() {
 
-    this.storage.get('logeado').then((val) => {
-        console.log("Se puede ingresar con logeado");
-        console.log(val);
-        if(val == true){
-          this.storage.get('obj').then((objeto) => {
-            console.log(objeto);
-            this.usuariosPdr.guardarUsuario(objeto);
-            this.navCtrl.setRoot(TabsPage);
-          });
-        }else{
-          let cargando = this.loadCtrl.create({content:"Espere, cargando sucursales"});
-          cargando.present();
-          this.sucursalesPrd.gets().subscribe(datos => {
-              this.arreglo = datos;
-              cargando.dismiss();
-          },error => {
-            cargando.dismiss();
-            let mensajeError = this.toasCtrl.create({message:"Error al cargar las sucursales,\nNota: Cerrar la aplicación",closeButtonText:"Cerrar",showCloseButton:true});
-            mensajeError.present();
-          });
-        }
+    let obj = {
+      login: "Cocinero",
+      password: "12345",
+      idSucursal: 3
+    }
+
+    this.usuariosPdr.ingresarSistema(obj).subscribe(datos => {
+      let ingresar = datos.entrar;
+      if (ingresar == true) {
+        let toas = this.toasCtrl.create({ message: "Sistema ingresado con exito", duration: 1500 });
+        toas.present();
+        this.usuariosPdr.guardarUsuario(datos);
+        this.storage.set('obj', datos);
+        this.storage.set('logeado', true);
+        console.log(datos);
+        this.navCtrl.setRoot(TabsPage);
+      } else {
+        let toas = this.toasCtrl.create({ message: "Usuario / Contraseña invalidos", duration: 1500 });
+        toas.present();
+      }
     });
 
 
-    
+
   }
 
-  public ingresar(usuario,contra):any{
+  public ingresar(usuario, contra): any {
     let strUsuario = usuario.value;
     let strContra = contra.value;
-    let toas = this.toasCtrl.create({message:"Usuario y/o Contraseña invalidos",duration:2000});
-    
-    if(strUsuario.trim() == ""){
-       this.invalido = true;
-       toas.present();
-       return;
+    let toas = this.toasCtrl.create({ message: "Usuario y/o Contraseña invalidos", duration: 2000 });
+
+    if (strUsuario.trim() == "") {
+      this.invalido = true;
+      toas.present();
+      return;
     }
 
-    if(strContra.trim() == ""){
+    if (strContra.trim() == "") {
       this.invalido = true;
       toas.present();
       return;
     }
 
     let obj = {
-      login:strUsuario,
-      password:strContra,
-      idSucursal:this.sucursal
+      login: "Admin",
+      password: "12345",
+      idSucursal: 3
     }
 
-    this.usuariosPdr.ingresarSistema(obj).subscribe(datos =>{
-        let ingresar = datos.entrar;
-        if(ingresar == true){
-          let toas = this.toasCtrl.create({message:"Sistema ingresado con exito",duration:1500});
-          toas.present();
-          this.usuariosPdr.guardarUsuario(datos);
-          this.storage.set('obj',datos);
-          this.storage.set('logeado',true);
-          console.log(datos);
-          this.navCtrl.setRoot(TabsPage);
-        }else{
-          let toas = this.toasCtrl.create({message:"Usuario / Contraseña invalidos",duration:1500});
-          toas.present();
-        }
+    this.usuariosPdr.ingresarSistema(obj).subscribe(datos => {
+      let ingresar = datos.entrar;
+      if (ingresar == true) {
+        let toas = this.toasCtrl.create({ message: "Sistema ingresado con exito", duration: 1500 });
+        toas.present();
+        this.usuariosPdr.guardarUsuario(datos);
+        this.storage.set('obj', datos);
+        this.storage.set('logeado', true);
+        console.log(datos);
+        this.navCtrl.setRoot(TabsPage);
+      } else {
+        let toas = this.toasCtrl.create({ message: "Usuario / Contraseña invalidos", duration: 1500 });
+        toas.present();
+      }
     });
   }
 
